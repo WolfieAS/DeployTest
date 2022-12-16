6de3733aa7fecb30923203761d53fd99c24310a0
Kamil Jajuga <k.jajuga@joser-erbacher.de>
2022-12-16 18:02:37 +0100

    login + mainmenu

--- Projekt/main.py
+++ Projekt/main.py
@@ -11,13 +11,26 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        username = request.form['uname']
+        password = request.form['psw']      #Hier muss das passwort noch geprueft werden
+        
+        #pruefung DB
+        #erstellung der User Session
+        return redirect(url_for('mainmenu'))
+
     users = db.getAllUsers()
-    return render_template('login.html', users = users)
+    return render_template('login.html', users=users)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     users = db.addUser("name1", "password1", "email1")
     return render_template('login.html')
+
+@app.route('/mainmenu', methods=['GET', 'POST'])
+def mainmenu():
+    #Pruefung der User Session, ansonsten /login
+    return render_template('mainmenu.html')
 
 
 if __name__ == '__main__':
