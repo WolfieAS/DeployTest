376b8792ea8303006e20476a779ace63712ef90a
Kamil Jajuga <k.jajuga@joser-erbacher.de>
2022-12-21 21:07:28 +0100

    bad request error handler (Custom error page)

--- Projekt/main.py
+++ Projekt/main.py
@@ -80,5 +80,9 @@ def logout():
     session.pop("user", None)
     return redirect(url_for('index'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
